f2fc86b9f4292e0f875601cdd120c2336eed9c4c
AlbertoSoriano <albertosorianogomez1@gmail.com>
2023-11-06 20:15:28 +0100

    Creado el metodo get_url

--- bicimad/UrlEMT.py
+++ bicimad/UrlEMT.py
@@ -12,12 +12,21 @@ class UrlEMT:
 
     @staticmethod
     def get_links(html) -> set:
-        patron = r'href="/getattachment/[^"]+/trips_\d{2}_\d{2}_[A-Za-z]+-csv\.aspx"'
+        """
+
+        :param html:
+        :return:
+        """
+        patron = r'/getattachment/[^"]+/trips_\d{2}_\d{2}_[A-Za-z]+-csv\.aspx'
         datare = re.compile(patron)
         matches = datare.findall(html)
         return set(matches)
 
     def select_valid_urls(self):
+        """
+
+        :return:
+        """
         url = self.EMT + self.GENERAL
         response = requests.get(url)
 
@@ -26,3 +35,39 @@ class UrlEMT:
             self.set_valid_urls = valid_urls
         else:
             raise ConnectionError("The connection was not successful")
+
+    def get_url(self, month: int, year: int) -> str:
+        """
+
+        :param month:
+        :param year:
+        :return:
+        """
+
+        # If not in the selected range an error is raised
+        if month < 1 or month > 12 or year < 21 or year > 23:
+            raise ValueError("The month must be between 1 and 12 and the year between 21 and 23")
+
+        # Fill with zeros the month and year
+        month_str = str(month).zfill(2)
+        year_str = str(year).zfill(2)
+
+        # Creates de url to be checked in the set
+        url = f"trips_{year_str}_{month_str}"
+
+        # check if the selected url is in the set
+        for urls in self.set_valid_urls:
+            if url in urls:
+                return self.EMT + urls
+
+        # if not found an error is raised
+        raise ValueError("Month and year not found")
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     cosa.select_valid_urls()
     var = cosa.set_valid_urls
     print(var)
+    url = cosa.get_url(1, 23)
+    print(url)
